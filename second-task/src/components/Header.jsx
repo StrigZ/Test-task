fdfa77c75eadd5110a4931d34198d6852c5af396
StrigZ <mirgasan2010@ya.ru>
2022-11-25 23:30:41 +0300

    add main section

--- second-task/src/components/Header.jsx
+++ second-task/src/components/Header.jsx
@@ -7,44 +7,48 @@ const Header = () => {
   return (
     <>
       <header className={styles.header}>
-        <div className={styles["header__top"]}>
-          <LogoSVG />
-          <div>
-            <ul className={styles["header__top--socials"]}>
-              <li>
-                <a href="#">
-                  <Telegram />
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <Viber />
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <Whatsapp />
-                </a>
-              </li>
-            </ul>
+        <div className="container">
+          <div className={styles["header__top"]}>
+            <LogoSVG />
+            <div>
+              <ul className={styles["header__top--socials"]}>
+                <li>
+                  <a href="#">
+                    <Telegram />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <Viber />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <Whatsapp />
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </header>
       <header className={styles["header__nav"]}>
-        <ul className={styles["header__menu"]}>
-          <li>
-            <a href="#">Услуги</a>
-          </li>
-          <li>
-            <a href="#">Виджеты</a>
-          </li>
-          <li>
-            <a href="#">Интеграции</a>
-          </li>
-          <li>
-            <a href="#">Кейсы</a>
-          </li>
-        </ul>
+        <div className="container">
+          <ul className={styles["header__menu"]}>
+            <li>
+              <a href="#">Услуги</a>
+            </li>
+            <li>
+              <a href="#">Виджеты</a>
+            </li>
+            <li>
+              <a href="#">Интеграции</a>
+            </li>
+            <li>
+              <a href="#">Кейсы</a>
+            </li>
+          </ul>
+        </div>
       </header>
     </>
   );
--- second-task/src/components/Main.jsx
+++ second-task/src/components/Main.jsx
@@ -1,6 +1,27 @@
 import styles from "./Main.module.css";
 
 const Main = () => {
-  return <main className={styles.main}>Main</main>;
+  return (
+    <main className={styles.main}>
+      <div className="container">
+        <h1 className={styles["main__title"]}>
+          Зарабатывайте больше
+          <span>с WELBEX</span>
+        </h1>
+        <p className={styles["main__description"]}>
+          Развиваем и контролируем продажи за вас
+        </p>
+        <p className={styles["main__features"]}>
+          Вместе <span>с бесплатной консультацией</span> мы дарим:
+        </p>
+        <ul className={styles["main__features--list"]}>
+          <li>Skype аудит</li>
+          <li>30 виджетов</li>
+          <li>Dashboard</li>
+          <li>Месяц аmoCRM</li>
+        </ul>
+      </div>
+    </main>
+  );
 };
 export default Main;
