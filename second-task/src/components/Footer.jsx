9d083ebaa1a0a77c8e276ff53cfb083aadc54409
StrigZ <mirgasan2010@ya.ru>
2022-11-26 19:21:00 +0300

    add footer mobile design

--- second-task/src/components/Footer.jsx
+++ second-task/src/components/Footer.jsx
@@ -6,89 +6,100 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className="container">
-        <div className={styles["footer__about"]}>
-          <h3 className={styles["footer-title"]}>О компании</h3>
-          <ul className={styles["footer__about--list"]}>
-            <li>
-              <a href="#">Партнёрская программа</a>
-            </li>
-            <li>
-              <a href="#">Вакансии</a>
-            </li>
-          </ul>
-        </div>
-        <div className={styles["footer__menu"]}>
-          <h3 className={styles["footer-title"]}>Меню</h3>
-          <div className={styles["footer__menu--box"]}>
-            <ul className={styles["footer__menu--list"]}>
-              <li>
-                <a href="#">Расчёт стоимости</a>
-              </li>
-              <li>
-                <a href="#">Услуги</a>
-              </li>
-              <li>
-                <a href="#">Виджеты</a>
-              </li>
-              <li>
-                <a href="#">Интеграции</a>
-              </li>
-              <li>
-                <a href="#">Наши клиенты</a>
-              </li>
-            </ul>
-            <ul className={styles["footer__menu--list"]}>
-              <li>
-                <a href="#">Благодарность клиентов</a>
-              </li>
-              <li>
-                <a href="#">Кейсы</a>
-              </li>
-              <li>
-                <a href="#">Сертификаты</a>
-              </li>
-              <li>
-                <a href="#">Блог на Youtube</a>
-              </li>
-              <li>
-                <a href="#">Вопрос / Ответ</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className={styles["footer__contact"]}>
-          <h3 className={styles["footer-title"]}>Контакты</h3>
-          <ul className={styles["footer__contact--list"]}>
-            <li>+7 555 555-55-55</li>
-            <li>
-              <ul className={styles["footer__contact--socials"]}>
+        <div className={styles["footer__inner"]}>
+          <div className={styles["footer__inner--top"]}>
+            <div className={styles["footer__about"]}>
+              <h3 className={styles["footer-title"]}>О компании</h3>
+              <ul className={styles["footer__about--list"]}>
                 <li>
-                  <a href="#">
-                    <Telegram />
-                  </a>
+                  <a href="#">Партнёрская программа</a>
                 </li>
                 <li>
-                  <a href="#">
-                    <Viber />
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <Whatsapp />
-                  </a>
+                  <a href="#">Вакансии</a>
                 </li>
               </ul>
-            </li>
-            <li>Москва, Путевой проезд 3с1, к 902</li>
-          </ul>
-        </div>
-        <div className={styles["footer__copyright"]}>
-          <ul>
-            <li>&#169;WELBEX 2022. Все права защищены.</li>
-            <li>
-              <a href="#">Политика конфиденциальности</a>
-            </li>
-          </ul>
+            </div>
+            <div className={styles["footer__menu"]}>
+              <h3 className={styles["footer-title"]}>Меню</h3>
+              <div className={styles["footer__menu--box"]}>
+                <ul className={styles["footer__menu--list"]}>
+                  <li>
+                    <a href="#">Расчёт стоимости</a>
+                  </li>
+                  <li>
+                    <a href="#">Услуги</a>
+                  </li>
+                  <li>
+                    <a href="#">Виджеты</a>
+                  </li>
+                  <li>
+                    <a href="#">Интеграции</a>
+                  </li>
+                  <li>
+                    <a href="#">Наши клиенты</a>
+                  </li>
+                </ul>
+                <ul className={styles["footer__menu--list"]}>
+                  <li>
+                    <a className={styles.mobile} href="#">
+                      Благодарность клиентов
+                    </a>
+                    <a className={styles.desktop} href="#">
+                      Благодарственные письма
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">Кейсы</a>
+                  </li>
+                  <li>
+                    <a href="#">Сертификаты</a>
+                  </li>
+                  <li>
+                    <a href="#">Блог на Youtube</a>
+                  </li>
+                  <li>
+                    <a href="#">Вопрос / Ответ</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className={styles["footer__contact"]}>
+              <h3 className={styles["footer-title"]}>Контакты</h3>
+              <ul className={styles["footer__contact--list"]}>
+                <li>+7 555 555-55-55</li>
+                <li>
+                  <ul className={styles["footer__contact--socials"]}>
+                    <li>
+                      <a href="#">
+                        <Telegram />
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <Viber />
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <Whatsapp />
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                <li>Москва, Путевой проезд 3с1, к 902</li>
+              </ul>
+            </div>
+          </div>
+          <div className={styles["footer__inner--bottom"]}>
+            <div className={styles["footer__copyright"]}>
+              <ul>
+                <li>&#169;WELBEX 2022. Все права защищены.</li>
+                <li>
+                  <a href="#">Политика конфиденциальности</a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </footer>
